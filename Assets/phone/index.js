92359efac7437d8731f0d1dd79567f8e9ad7cac8
green-Desktop <appieosugood@gmail.com>
2024-01-28 01:43:48 +0800

    if have https it maybe work

--- Assets/phone/index.js
+++
@@ -1,8 +0,0 @@
-function c() {
-    alert('click');
-        fetch("http://localhost:13579/HeartBeat",
-        {mode:'cors'})
-      .then((response) => {let json = response.json(); alert(json);return json;})
-      .then((json) => alert(json));
-}
-
