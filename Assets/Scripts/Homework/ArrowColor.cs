78097598bf4418910bba4b99c9163b3cce01e8a1
green-Desktop <appieosugood@gmail.com>
2024-01-27 17:08:02 +0800

    add arrow prefab

---
+++ Assets/Scripts/Homework/ArrowColor.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace.Homework
+{
+    [CreateAssetMenu(fileName = "ArrowColor", menuName = "ScriptableObject/ArrowColor", order = 0)]
+    public class ArrowColor : ScriptableObject
+    {
+        public Color upColor;
+        public Color downColor;
+        public Color leftColor;
+        public Color rightColor;
+    }
+}
