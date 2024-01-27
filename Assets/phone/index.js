c151462ac485ca3be52ba700e27e47ef79279d2d
green-Desktop <appieosugood@gmail.com>
2024-01-28 00:23:12 +0800

    more alert

--- Assets/phone/index.js
+++ Assets/phone/index.js
@@ -2,8 +2,7 @@ function c() {
     alert('click');
         fetch("http://localhost:13579/HeartBeat",
         {mode:'cors'})
-      .then((response) => response.json())
-      .then((json) => console.log(json));
-
+      .then((response) => {let json = response.json(); alert(json);return json;})
+      .then((json) => alert(json));
 }
 
