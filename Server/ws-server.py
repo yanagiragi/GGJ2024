7fdc65df19a3ae194022bfef86f7cc39a0d54339
yanagiragi <qaz90473@gmail.com>
2024-01-28 03:28:43 +0800

    feat: Reuse player id in webSocket server

--- Server/ws-server.py
+++ Server/ws-server.py
@@ -2,18 +2,27 @@
 import asyncio
 import websockets
 
+users = {}
+userCount = 1
+
 async def echo(websocket, path):
+    global users
+    global userCount
+    
     async for message in websocket:
         try:
             print('received from client:', message)
 
             if message.startswith('login?id='):
                 id = message[len('login?id='):]
-                response = f'Hello, {id}'
+                if not id in users:
+                    users[id] = f'Player {userCount}'
+                    userCount += 1
+                response = f'Hello, {users[id]}'
             
             elif message.startswith('slap?id='):
                 id = message[len('slap?id='):]
-                response = f'Detect Slap from {id}'
+                response = f'Detect Slap from {users[id]}'
 
             await websocket.send(response)
         except:
