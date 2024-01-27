f29446b3582fa41dcc9cae811ab65412147b9491
yanagiragi <qaz90473@gmail.com>
2024-01-28 04:07:01 +0800

    feat: Show qrcode to connect to web socket easily

--- Server/http-server.py
+++ Server/http-server.py
@@ -1,13 +1,16 @@
 # -*- coding: utf-8 -*-
 import socket
 from flask import Flask, render_template
+import segno
+import os
 
 # https://stackoverflow.com/questions/166506/finding-local-ip-addresses-using-pythons-stdlib
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.connect(("8.8.8.8", 80))
+s.connect(('8.8.8.8', 80))
 ip = s.getsockname()[0]
 s.close()
-print(ip)
+
+port = '13579'
 
 app = Flask(__name__)
 
@@ -15,5 +18,14 @@ app = Flask(__name__)
 def home():
    return render_template('index.html', ip = ip)
 
-if __name__ == "__main__":
-    app.run(host = '0.0.0.0', port='13579')
+if __name__ == '__main__':
+    url = f'http://{ip}:{port}/'
+    print(f'Connect {url} to open the webpage.')
+    
+    qrcode_path = 'qrcode.png'
+    qrcode = segno.make_qr(url)
+    qrcode.save(qrcode_path, scale=10)
+    
+    os.system(qrcode_path)
+
+    app.run(host = '0.0.0.0', port=port)
