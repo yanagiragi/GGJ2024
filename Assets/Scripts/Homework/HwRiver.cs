5ef04ede0c4f739d79aee4d2a4b15eaa466845dd
green-Desktop <appieosugood@gmail.com>
2024-01-28 11:12:55 +0800

    arrow anmation

---
+++ Assets/Scripts/Homework/HwRiver.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace Homework
+{
+    public class HwRiver : MonoBehaviour
+    {
+        private GameObject Keyboard;
+        private GameObject leftArrow;
+        private GameObject rightArrow;
+
+        private void Awake()
+        {
+            leftArrow = transform.Find("hint/LeftArrow").gameObject;
+            rightArrow = transform.Find("hint/RightArrow").gameObject;
+            Keyboard = transform.Find("hint/Keyboard").gameObject;
+        }
+
+        private void Update()
+        {
+            UpdateArrowAnimation();
+        }
+
+        private void UpdateArrowAnimation()
+        {
+            var secFromStart = Time.timeSinceLevelLoad;
+            var angle = secFromStart * 3.14f;
+            var tosin = Mathf.Abs(Mathf.Sin(angle)) + 0.5f;
+            leftArrow.transform.localScale = new Vector3(tosin, tosin, 1);
+            rightArrow.transform.localScale = new Vector3(tosin, tosin, 1);
+        }
+    }
+}
